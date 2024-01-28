b71680a49594c1e3a8f08b30c2ea5fbcc9ba0fb5
Aman <amanmehta4411@gmail.com>
2024-01-28 11:29:19 +0530

    edit and delete actions

--- StudentManagement/add.php
+++ StudentManagement/add.php
@@ -3,13 +3,28 @@
 
 <?php
 
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $editFlag = isset($_REQUEST['editedStudent']) ? true : false;
+    $rollNumber = isset($_REQUEST['editedStudent']) ? $_REQUEST['editedStudent'] : null;
+    if($editFlag){
+        $sql = "SELECT * FROM students WHERE RollNumber=$rollNumber";
+        $result = mysqli_query($conn, $sql);
+        $student = mysqli_fetch_assoc($result);
+    }
+}
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $name = $_POST['name'];
     $class = $_POST['class'];
-
-    $sql = "INSERT INTO students (Name, Class) VALUES ('$name', '$class')";
+    $rollNumber = $_POST['submitBtn'];
+    if($rollNumber){
+        $sql = "UPDATE students SET Name='$name',Class='$class' WHERE RollNumber = $rollNumber";
+    }else{
+        $sql = "INSERT INTO students (Name, Class) VALUES ('$name', '$class')";
+    }
+    
     if(mysqli_query($conn, $sql)){
-        print_r("Student record Inserted Successfully");
+        print_r("Student record" . $rollNumber ? "Updated" : "Inserted" . "Successfully");
     }else{
         print_r("Error inserting student record" . mysqli_error($conn));
     }
@@ -24,7 +39,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     <div class="row">
         <div>
-            <h4>Add Student Details</h4>
+            <h4><?php echo $editFlag ? "Edit Student Details" : "Add Student Details" ?></h4>
         </div>
         <br>
         <br>
@@ -33,14 +48,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form action="" method="POST">
             <div class="form-group">
                 <label for="name">Student Name</label>
-                <input type="text" class="form-control" id="name" placeholder="Enter student name" name="name">
+                <input type="text" class="form-control" id="name" placeholder="Enter student name" name="name" value="<?php echo isset($student['Name']) ? $student['Name'] : "" ?>">
             </div>
             <div class="form-group">
                 <label for="lass">Student Class</label>
-                <input type="text" class="form-control" id="class" placeholder="Enter student class" name="class">
+                <input type="text" class="form-control" id="class" placeholder="Enter student class" name="class" value="<?php echo isset($student['Class']) ? $student['Class'] : "" ?>">
             </div>
             <br>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary" name="submitBtn" value="<?php echo $rollNumber?>">Submit</button>
         </form>
 </div>
 
--- StudentManagement/index.php
+++ StudentManagement/index.php
@@ -1,6 +1,14 @@
 <?php include_once("/xampp/htdocs/IntroductionToPhp/StudentManagement/common/header.php") ?>
 <?php include_once("/xampp/htdocs/IntroductionToPhp/StudentManagement/common/db.php") ?>
 
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $rollNumber = $_POST['deletedStudent'];
+    $sql = "DELETE FROM students WHERE RollNumber=$rollNumber";
+    mysqli_query($conn,$sql);
+}
+?>
+
 <div class="container">
     <br>
     <br>
@@ -21,6 +29,8 @@
                 <th scope="col">Name</th>
                 <th scope="col">Class</th>
                 <th scope="col">Created</th>
+                <th>Edit Action</th>
+                <th>Delete Action</th>
             </tr>
         </thead>
         <tbody>
@@ -34,6 +44,8 @@
                     <td><?php echo $student['Name'] ?></td>
                     <td><?php echo $student['Class'] ?></td>
                     <td><?php echo $student['Created'] ?></td>
+                    <td><form action="add.php" method="get"><button type="submit" class="btn btn-warning" name="editedStudent" value="<?php echo $student['RollNumber'] ?>">Edit</button></form></td>
+                    <td><form action="" method="post"><button type="submit" class="btn btn-danger" name="deletedStudent" value="<?php echo $student['RollNumber'] ?>">Delete</button></form></td>
                 </tr>
                 <?php
               }
